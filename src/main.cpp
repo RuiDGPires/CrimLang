23210a870796865d33ddc12a4e4e8ed1b7b7a828
Rui Pires <pires.rui@outlook.pt>
2021-10-10 17:50:32 +0100

    Disable AST printing

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[]){
 	try{
 		//print_token_vec(crl::tokenize(argv[1]));
 		crl::Ast *ast = crl::generate_ast(crl::tokenize(argv[1]));
-		std::cout << ast->to_string() << std::endl;
+		//std::cout << ast->to_string() << std::endl;
 		crl::semantic_check(ast);
 		//generate_cas(ast, "b.cas");
 		delete ast;
